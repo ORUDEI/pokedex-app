101df3d6d46618f1b1f9f18903fd7c723c1b697a
ORUDEI <ald.jo.a@gmail.com>
2023-12-16 10:32:25 -0300

    get pokemon info

--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -1,23 +1,58 @@
 import { createContext, useState } from 'react'
+import {
+  formatTypes,
+  formatAbilities,
+  formatStats,
+  getPokemonDescription,
+  getEvolutions,
+} from '../helpers/pokemon.js'
+import axios from 'axios'
 
 const PokemonContext = createContext()
 
 const PokemonProvider = ({ children }) => {
-    const [showDetailPokemon,setShowDetailPokemon] = useState(false)
+  const [pokemonDetail, setPokemonDetail] = useState(null)
+  const [showDetailPokemon, setShowDetailPokemon] = useState(false)
 
-    const showPokemonById = () => {
-        setShowDetailPokemon(true)
-    }
+  const showPokemon = async (pokemonInfo) => {
+    const { data: dataSpecies } = await axios.get(pokemonInfo.species.url)
+    const { data: dataEvolution } = await axios.get(
+      dataSpecies.evolution_chain.url
+    )
 
-    const closePokemonDetail = () => {
-        setShowDetailPokemon(false)
-    }
+    const { id, name, height, weight, stats, types, abilities } = pokemonInfo
 
-  return <PokemonContext.Provider value={{
-    showDetailPokemon,
-    showPokemonById,
-    closePokemonDetail
-  }}>{children}</PokemonContext.Provider>
+    setPokemonDetail({
+      id,
+      name,
+      height,
+      weight,
+      stats: formatStats(stats),
+      types: formatTypes(types),
+      abilities: formatAbilities(abilities),
+      description: getPokemonDescription(dataSpecies),
+      evolutions: getEvolutions(dataEvolution),
+    })
+
+    setShowDetailPokemon(true)
+  }
+
+  const closePokemonDetail = () => {
+    setShowDetailPokemon(false)
+  }
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        showDetailPokemon,
+        showPokemon,
+        closePokemonDetail,
+        pokemonDetail,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  )
 }
 
 export { PokemonContext, PokemonProvider }
